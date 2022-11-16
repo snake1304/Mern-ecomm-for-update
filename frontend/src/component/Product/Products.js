060cf02c5f0bbb2a4c75dedb5c5ee3cb5c609a62
snake1304 <13giaminh13@gmail.com>
2022-11-16 10:26:59 +0700

    initial commit

---
+++ frontend/src/component/Product/Products.js
@@ -0,0 +1,128 @@
+import React, { Fragment, useEffect, useState } from "react";
+import "./Products.css";
+import { useDispatch, useSelector } from "react-redux";
+import { clearErrors, getProduct } from "../../actions/productAction";
+import Loader from "../layout/Loader/Loader";
+import ProductCard from "../Home/ProductCard";
+import Panigation from "react-js-pagination";
+import { Slider } from "@mui/material";
+import { useAlert } from "react-alert";
+import MetaData from "../layout/MetaData";
+
+const categories = [
+  "Birthdays",
+  "Anniversary",
+  "Congratulations",
+  "Best wishes",
+  "Christmas",
+  "Romance",
+  "Anniversary",
+];
+
+const Products = ({ match }) => {
+  const dispatch = useDispatch();
+
+  const alert = useAlert();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([0, 25000]);
+  const [category, setCategory] = useState("");
+
+  const [ratings, setRatings] = useState(0);
+
+  const {
+    products,
+    loading,
+    error,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
+
+  const keyword = match.params.keyword;
+
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e);
+  };
+
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
+  };
+  let count = filteredProductsCount;
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    dispatch(getProduct(keyword, currentPage, price, category, ratings));
+  }, [dispatch, keyword, currentPage, price, category, ratings, alert, error]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="PRODUCTS -- ECOMMERCE" />
+          <h2 className="productsHeading">Products</h2>
+
+          <div className="products">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
+
+          <div className="filterBox">
+            <h3>Price</h3>
+            <Slider
+              trackColor="#F85F68"
+              size="small"
+              value={price}
+              onChange={priceHandler}
+              aria-label="Small"
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+              min={0}
+              max={6000}
+            />
+            <h3>Categories</h3>
+            <ul className="categoryBox">
+              {categories.map((category) => (
+                <li
+                  className="category-link"
+                  key={category}
+                  onClick={() => setCategory(category)}
+                >
+                  {category}
+                </li>
+              ))}
+            </ul>
+          </div>
+          {resultPerPage < productsCount && (
+            <div className="panigationBox">
+              <Panigation
+                activePage={currentPage}
+                itemsCountPerPage={resultPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText="Next"
+                prevPageText="Prev"
+                firstPageText="1st"
+                lastPageText="Last"
+                itemClass="page-item"
+                linkClass="page-link"
+                activeClass="pageItemActive"
+                activeLinkClass="pageLinkActive"
+              />
+            </div>
+          )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Products;
---
+++ frontend/src/component/layout/Contact.js
@@ -0,0 +1,90 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { useAlert } from "react-alert";
+import "./contact.css";
+
+const Contact = ({ history }) => {
+  const form = useRef();
+  const alert = useAlert();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_497hbog",
+        "template_4fuwdbn",
+        form.current,
+        "H1_tOpdXCim5MmBvB"
+      )
+      .then(
+        (result) => {
+          history.push("/");
+          alert.success(result.text);
+        },
+        (error) => {
+          alert.success(error.text);
+        }
+      );
+  };
+
+  return (
+    // <form ref={form} onSubmit={sendEmail}>
+    //   <label>Name</label>
+    //   <input type="text" name="user_name" />
+    //   <label>Email</label>
+    //   <input type="email" name="user_email" />
+    //   <label>Message</label>
+    //   <textarea name="message" />
+    //   <input type="submit" value="Send" />
+    // </form>
+    <>
+      <div className="about-sectionAbout">
+        <div className="inner-widthAbout">
+          <h1>Contact Us</h1>
+
+          <div id="form-div">
+            <form ref={form} onSubmit={sendEmail} className="form" id="form1">
+              <p className="name">
+                <input
+                  name="user_name"
+                  type="text"
+                  className="validate[required,custom[onlyLetter],length[0,100]] feedback-input"
+                  placeholder="Name"
+                  id="name"
+                />
+              </p>
+
+              <p className="email">
+                <input
+                  name="user_email"
+                  type="text"
+                  className="validate[required,custom[email]] feedback-input"
+                  id="email"
+                  placeholder="Email"
+                />
+              </p>
+
+              <p className="text">
+                <textarea
+                  name="message"
+                  resize="none"
+                  className="validate[required,length[6,300]] feedback-input"
+                  id="comment"
+                  rows="10"
+                  placeholder="Message"
+                ></textarea>
+              </p>
+
+              <div className="submit">
+                <input type="submit" value="Send" id="button-blue" />
+                <div className="ease"></div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
